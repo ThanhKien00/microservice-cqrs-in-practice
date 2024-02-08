a4bbc46db06c644bd9eb74d2cd72782773500730
ThanhKien00 <kien.nt112002@gmail.com>
2024-02-08 15:02:45 +0700

    commit code

--- cqrs-core/src/main/java/com/techbank/cqrs/command/BaseCommand.java
+++ cqrs-core/src/main/java/com/techbank/cqrs/command/BaseCommand.java
@@ -2,8 +2,10 @@ package com.techbank.cqrs.command;
 
 import com.techbank.cqrs.message.Message;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 public abstract class BaseCommand extends Message {

7e152917497c812a4ef655c65c92b88f6853b0bc
ThanhKien00 <kien.nt112002@gmail.com>
2024-02-08 14:12:15 +0700

    commit code

---
+++ account-services/account.cmd/src/main/java/com/techbank/cmd/AccountCommandApplication.java
@@ -0,0 +1,13 @@
+package com.techbank.cmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountCommandApplication.class, args);
+	}
+
+}
---
+++ account-services/account.cmd/src/test/java/com/techbank/cmd/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbank.cmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
